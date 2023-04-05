fb6af36ec49808d285bde769805972977b30cf9f
imjayapple <imjayapple@gmail.com>
2023-04-04 20:53:09 -0700

    Initial commit, two functions that return the appropriate conversion of input temp (C or F) to output temp (C to F or F to C)

---
+++ temperature.py
@@ -0,0 +1,6 @@
+# Two functions for the conversion for C to F, and F to C
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
