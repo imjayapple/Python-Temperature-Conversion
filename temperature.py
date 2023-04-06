3683797c566ece47d41b94517253fa1a407e49df
imjayapple <imjayapple@gmail.com>
2023-04-05 20:57:45 -0700

    Main function, two while loops, one for temperature value and one for unit value (C or F)

--- temperature.py
+++ temperature.py
@@ -4,3 +4,15 @@ def celsius_to_fahrenheit(celsius):
 
 def fahrenheit_to_celsius(fahrenheit):
     return (fahrenheit - 32) * 5/9
+
+
+# While loop to obtain a valid termperature value from the user
+def main():
+    print("Welcome to the Temperature Converter!")
+
+    while True:
+        try:
+            temp_value = float(input("\nEnter a temperature value: "))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
