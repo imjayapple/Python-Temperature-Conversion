f87e101d6ae2ea22015a235f1b874fc473114775
imjayapple <imjayapple@gmail.com>
2023-04-07 21:41:03 -0700

    If-else block, pass the user input to the proper function, print the result

--- temperature.py
+++ temperature.py
@@ -16,10 +16,24 @@ def main():
             break
         except ValueError:
             print("Invalid input. Please enter a valid number.")
+
     # While loop to obtain the value unit of temp from the user
+
     while True:
         unit = input("Enter the unit of the temperature value (C for Celsius or F for Fahrenheit): ").upper()
         if unit == 'C' or unit == 'F':
             break
         else:
             print("Invalid input. Please enter either 'C' or 'F'.")
+
+    # If-Else block, pass the user input to the proper function, print the result
+
+    if unit == 'C':
+        converted_value = celsius_to_fahrenheit(temp_value)
+        print(f"\n{temp_value}° Celsius is equal to {converted_value:.2f}° Fahrenheit.")
+    else:
+        converted_value = fahrenheit_to_celsius(temp_value)
+        print(f"\n{temp_value}° Fahrenheit is equal to {converted_value:.2f}° Celsius.")
+
+if __name__ == "__main__":
+    main()
